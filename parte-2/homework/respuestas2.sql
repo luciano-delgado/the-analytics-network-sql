3ea2bacc2b7092861d856afe8fa8e164352a2080
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-04-12 21:09:06 -0300

    Update respuestas2.sql

--- parte-2/homework/respuestas2.sql
+++ parte-2/homework/respuestas2.sql
@@ -12,7 +12,8 @@ cast(fecha as date),
 conteo
 from stg.super_store_count
 -- 2 Recibimos otro archivo con ingresos a tiendas de meses anteriores. Ingestar el archivo y agregarlo a la vista del ejercicio anterior (Ejercicio 1 Clase 6). Cual hubiese sido la diferencia si hubiesemos tenido una tabla? (contestar la ultima pregunta con un texto escrito en forma de comentario)
--- 3 Crear una vista con el resultado del ejercicio de la Parte 1 - Clase 3 - Ejercicio 10, donde calculamos el margen bruto en dolares. Agregarle la columna de ventas, descuentos, y creditos en dolares para poder reutilizarla en un futuro.
+-- 3 Crear una vista con el resultado del ejercicio de la Parte 1 - Clase 3 - Ejercicio 10, donde calculamos el margen bruto en dolares. 
+Agregarle la columna de ventas, descuentos, y creditos en dolares para poder reutilizarla en un futuro.
  SELECT ols.orden,
     ols.producto,
     ols.tienda,
@@ -56,3 +57,15 @@ from stg.super_store_count
    FROM stg.order_line_sale ols
      LEFT JOIN stg.cost c1 ON c1.codigo_producto::text = ols.producto::text
      LEFT JOIN stg.monthly_average_fx_rate mfx ON EXTRACT(month FROM mfx.mes) = EXTRACT(month FROM ols.fecha);
+
+-- 4 Generar una query que me sirva para verificar que el nivel de agregacion de la tabla de ventas 
+(y de la vista) no se haya afectado. 
+Recordas que es el nivel de agregacion/detalle? Lo vimos en la teoria de la parte 1! 
+Nota: La orden M999000061 parece tener un problema verdad? Lo vamos a solucionar mas adelante.
+
+select count (*), count (distinct orden)
+from stg.vw_parte2_clase1_ej3
+--where orden = 'M999000061'
+union all
+select count(*) , count (distinct orden)
+from stg.order_line_sale
