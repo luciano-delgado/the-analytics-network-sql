43a6a7ba441a8476fb286e570ab8d7712d6ea7e8
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-05-01 16:11:24 -0300

    1/5: Clase 7 hasta ultimo ejercicio.
    Falta ver bien el 5 y el 6...

--- parte-2/homework/respuestas2.sql
+++ parte-2/homework/respuestas2.sql
@@ -209,3 +209,26 @@ GROUP BY
   fecha
 ORDER BY 
   fecha;
+  
+  --6) Crear una vista de inventario con la cantidad de inventario por dia, tienda y producto,
+with inv as (
+select 
+fecha,
+tienda,
+sku, (inicial + final )/2 as inv_por_dia
+from stg.inventory i -- 1887 filas totales
+)
+select 
+i.fecha,i.tienda,i.sku,i.inv_por_dia,
+i.inv_por_dia * c1.costo_promedio_usd as costo,
+pm.nombre, pm.categoria,
+sm.pais, sm.nombre,
+(select fecha from stg.inventory inv where inv.sku = i.sku order by fecha desc limit 1) last_snapshot,
+sum(venta) over (order by ols.fecha rows between 6 preceding and current row) as vta7
+from inv i 
+left join stg.product_master pm on pm.codigo_producto = i.sku
+left join stg.store_master sm on sm.codigo_tienda = i.tienda
+left join stg.cost c1 on c1.codigo_producto = i.sku
+left join stg.order_line_sale ols on ols.producto = i.sku and ols.fecha = i.fecha and ols.tienda = i.tienda
+-- where i.sku = 'p100014' and i.fecha = '2022-11-22'
+order by fecha asc
