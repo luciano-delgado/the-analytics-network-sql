138d9dbea7dc622718788c5f04c4e2ed91aea1ee
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-05-17 21:34:13 -0300

    Create store_master.sql

---
+++ parte-3/project/retail-data-warehouse/stg/store_master.sql
@@ -0,0 +1,13 @@
+CREATE TABLE stg.store_master
+                 (
+                              codigo_tienda  SMALLINT
+                            , pais           VARCHAR(100)
+                            , provincia      VARCHAR(100)
+                            , ciudad         VARCHAR(100)
+                            , direccion      VARCHAR(255)
+                            , nombre         VARCHAR(255)
+                            , tipo           VARCHAR(100)
+                            , fecha_apertura DATE
+                            , latitud        DECIMAL(10, 8)
+                            , longitud       DECIMAL(11, 8)
+                 );
