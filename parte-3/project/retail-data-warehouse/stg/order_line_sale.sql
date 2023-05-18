0e5321e4af5d7d24228dc3b36472c656b88c3543
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-05-17 21:35:06 -0300

    Create order_line_sale.sql

---
+++ parte-3/project/retail-data-warehouse/stg/order_line_sale.sql
@@ -0,0 +1,15 @@
+CREATE TABLE stg.order_line_sale
+                 (
+                              orden      VARCHAR(10)
+                            , producto   VARCHAR(10)
+                            , tienda     SMALLINT
+                            , fecha      date
+                            , cantidad   int
+                            , venta      decimal(18,5)
+                            , descuento  decimal(18,5)
+                            , impuestos  decimal(18,5)
+                            , creditos   decimal(18,5)
+                            , moneda     varchar(3)
+                            , pos        SMALLINT
+                            , is_walkout BOOLEAN
+                 );
