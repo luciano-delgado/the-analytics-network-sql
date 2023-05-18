3273025184bb42d6b5e4694167e409dd8d0b515d
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-05-17 21:35:35 -0300

    Create return_movements.sql

---
+++ parte-3/project/retail-data-warehouse/stg/return_movements.sql
@@ -0,0 +1,12 @@
+CREATE TABLE stg.return_movements
+                 (
+                              orden      	VARCHAR(10)
+                            , envio   		VARCHAR(10)
+			    , item 	        VARCHAR(10)
+                            , cantidad   	int
+			    , id_movimiento int  
+			    , desde 	        VARCHAR(50)
+			    , hasta 		VARCHAR(50)
+			    , recibido_por 	VARCHAR(10)
+                            , fecha      	date
+                 );
