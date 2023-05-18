84ac41cf29c65a7f01cb4481ac217bc043f99680
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-05-17 21:31:37 -0300

    Create cost.sql

---
+++ parte-3/project/retail-data-warehouse/stg/cost.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS stg.cost;
+    
+CREATE TABLE stg.cost
+                 (
+                              codigo_producto    VARCHAR(10)
+                            , costo_promedio_usd DECIMAL
+                 )
+                 ;
