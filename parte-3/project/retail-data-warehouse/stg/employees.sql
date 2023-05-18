16d5aa9c0932a2a7c1b89454dd29c4c8a3a7124e
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-05-17 21:35:22 -0300

    Create employees.sql

---
+++ parte-3/project/retail-data-warehouse/stg/employees.sql
@@ -0,0 +1,13 @@
+CREATE TABLE stg.employees (
+    id serial PRIMARY KEY,
+    nombre varchar(255),
+    apellido varchar(255),
+    fecha_entrada date,
+    fecha_salida date,
+    telefono varchar(255),
+    pais varchar(255),
+    provincia varchar(255),
+    codigo_tienda varchar(255),
+    posicion varchar(255)
+)
+;
