39ee53c43cd376d223fb81ef1d90564b91142270
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-05-17 21:33:53 -0300

    Create monthly_average_fx_rate.sql

---
+++ parte-3/project/retail-data-warehouse/stg/monthly_average_fx_rate.sql
@@ -0,0 +1,7 @@
+CREATE TABLE stg.monthly_average_fx_rate
+                 (
+                              mes                 DATE
+                            , cotizacion_usd_peso DECIMAL
+                            , cotizacion_usd_eur DECIMAL
+                            , cotizacion_usd_uru  DECIMAL
+                 );
