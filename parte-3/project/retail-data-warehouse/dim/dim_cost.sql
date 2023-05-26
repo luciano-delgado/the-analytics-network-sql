4629dd83e4504202eb68a40af846db6a6a1fdc5f
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-05-26 12:08:22 -0300

    26/5/23: Pendiente. Falta crear dim.product_master para FK

--- parte-3/project/retail-data-warehouse/dim/dim_cost.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_cost.sql
@@ -4,3 +4,14 @@ CREATE TABLE IF NOT EXISTS dim.cost(
 product_id varchar(10) PRIMARY KEY,
 cost_usd numeric
 );
+
+-- Tabla: dim.cost con PK y FK: Una vez que tenga dim.product_master
+DROP TABLE IF EXISTS dim.cost;
+CREATE TABLE IF NOT EXISTS dim.cost(
+product_id varchar(10) PRIMARY KEY, -- PK
+cost_usd numeric,
+	-- FK
+	CONSTRAINT fk_product_id_cost
+	FOREIGN KEY (product_id)
+	REFERENCES dim.product_master(codigo_producto)
+);
