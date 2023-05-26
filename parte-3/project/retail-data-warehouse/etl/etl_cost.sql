d482df31fb37dd7c92224f8c6b7b0877cdf3af7b
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-05-26 13:42:25 -0300

    26/5/23: SP cost + validacion product_master + log

--- parte-3/project/retail-data-warehouse/etl/etl_cost.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_cost.sql
@@ -1,8 +1,13 @@
-/*CREACION DEL SP (EL CUAL SE ALMACENA EN ETL) QUE TOMARÁ LOS DATOS DEL STG Y LLENARÁ DIM */
+/*CREACION DEL SP (EL CUAL SE ALMACENA EN ETL) QUE TOMARÁ LOS DATOS DEL STG Y LLENARÁ DIM 
+Agregado: Se agrega call.log() al final para que deje registro de fecha, tabla modificada y usuario
+*/
 create or replace procedure etl.dim_cost()
-language sql as $$
--- buscar codigo de producto existente en product_master y traer solo los que existen
--- insert basado en la nueva tabla/cte con los codigos que existen unicamente
+language plpgsql as $$
+DECLARE
+  usuario varchar(10) := current_user ;
+BEGIN
+  usuario := current_user;
+-- buscar codigo de producto existente en product_master y traer solo los que existen -> insert basado en la nueva tabla/cte con los codigos que existen unicamente
 with validacion as (
 select c1.codigo_producto, c1.costo_promedio_usd 
 from stg.cost c1
@@ -13,6 +18,8 @@ select codigo_producto, costo_promedio_usd from validacion  -- Fuente de datos
 on conflict (product_id) -- # Si existe, update registro
 	do update set
 	cost_usd = excluded.cost_usd; 
+	call etl.log(current_date,'dim.cost' ,usuario);
+END;
 $$;
 /*
 La idea luego es correr esto. En caso de que actualice el archivo, debería modificar solo los nuevos datos
