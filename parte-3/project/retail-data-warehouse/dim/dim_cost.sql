96231e1040ddab7ba6914492b0071a202df92e2c
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-05-26 12:11:51 -0300

    26/5/23

--- parte-3/project/retail-data-warehouse/dim/dim_cost.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_cost.sql
@@ -5,6 +5,24 @@ product_id varchar(10) PRIMARY KEY,
 cost_usd numeric
 );
 
+-- Tabla: dim.product_master
+CREATE TABLE dim.product_master
+                 (
+                              codigo_producto VARCHAR(255)
+                            , nombre          VARCHAR(255)
+                            , categoria       VARCHAR(255)
+                            , subcategoria    VARCHAR(255)
+                            , subsubcategoria VARCHAR(255)
+                            , material        VARCHAR(255)
+                            , color           VARCHAR(255)
+                            , origen          VARCHAR(255)
+                            , ean             bigint
+                            , is_active       boolean
+                            , has_bluetooth   boolean
+                            , talle           VARCHAR(255)
+                 );
+		 
+		 
 -- Tabla: dim.cost con PK y FK: Una vez que tenga dim.product_master
 DROP TABLE IF EXISTS dim.cost;
 CREATE TABLE IF NOT EXISTS dim.cost(
