4782b44fe15191f7378f50fd31e7797733a6ebe4
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-05-26 13:06:08 -0300

    26/5/23: SP con validacion

--- parte-3/project/retail-data-warehouse/etl/etl_cost.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_cost.sql
@@ -1,11 +1,21 @@
 /*CREACION DEL SP (EL CUAL SE ALMACENA EN ETL) QUE TOMARÁ LOS DATOS DEL STG Y LLENARÁ DIM */
-create procedure etl.dim_cost()
+create or replace procedure etl.dim_cost()
 language sql as $$
+-- buscar codigo de producto existente en product_master y traer solo los que existen
+-- insert basado en la nueva tabla/cte con los codigos que existen unicamente
+with validacion as (
+select c1.codigo_producto, c1.costo_promedio_usd 
+from stg.cost c1
+inner join stg.product_master pm on pm.codigo_producto = c1.codigo_producto
+)
 insert into dim.cost (product_id,cost_usd) -- Donde inserto
-select codigo_producto, costo_promedio_usd from stg.cost -- Fuente de datos
+select codigo_producto, costo_promedio_usd from validacion  -- Fuente de datos
 on conflict (product_id) -- # Si existe, update registro
 	do update set
 	cost_usd = excluded.cost_usd; 
 $$;
-/*La idea luego es correr esto. En caso de que actualice el archivo, debería modificar solo los nuevos datos*/
+/*
+La idea luego es correr esto. En caso de que actualice el archivo, debería modificar solo los nuevos datos
+Agregado: Se ajusta SP para que solo inserte/modifique sí o sólo sí existe en dim.product_master
+*/
 call etl.dim_cost() 
