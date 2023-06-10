25d71ef14a98a69b53872ce44f6f113fde81704d
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-10 13:02:30 -0300

    10/6: Se incluye ddl

--- parte-3/project/retail-data-warehouse/dim/dim_supplier.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_supplier.sql
@@ -1,1 +1,8 @@
 
+/*Crear tabla de suppliers*/
+DROP TABLE IF EXISTS dim.suppliers; 
+CREATE TABLE dim.suppliers(
+	codigo_producto varchar(255) PRIMARY KEY,
+	nombre varchar(255),
+	is_primary boolean
+);
