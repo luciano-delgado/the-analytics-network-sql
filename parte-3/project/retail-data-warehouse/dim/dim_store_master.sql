4778489389e4d6f464a30a69f5ede745f199176b
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-14 20:03:38 -0300

    14/6: Se incluye ddl

--- parte-3/project/retail-data-warehouse/dim/dim_store_master.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_store_master.sql
@@ -1,1 +1,14 @@
 
+CREATE TABLE dim.store_master
+                 (
+                              codigo_tienda  SMALLINT
+                            , pais           VARCHAR(100)
+                            , provincia      VARCHAR(100)
+                            , ciudad         VARCHAR(100)
+                            , direccion      VARCHAR(255)
+                            , nombre         VARCHAR(255)
+                            , tipo           VARCHAR(100)
+                            , fecha_apertura DATE
+                            , latitud        DECIMAL(10, 8)
+                            , longitud       DECIMAL(11, 8)
+                 );
