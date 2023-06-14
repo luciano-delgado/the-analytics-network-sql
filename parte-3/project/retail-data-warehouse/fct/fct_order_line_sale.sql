a9b0edbe647e30676886c1f6bdb4f25967a9ea54
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-14 20:21:43 -0300

    14/6: ddl

--- parte-3/project/retail-data-warehouse/fct/fct_order_line_sale.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_order_line_sale.sql
@@ -1,1 +1,16 @@
 
+CREATE TABLE fct.order_line_sale
+                 (
+                              orden      VARCHAR(10)
+                            , producto   VARCHAR(10)
+                            , tienda     SMALLINT
+                            , fecha      date
+                            , cantidad   int
+                            , venta      decimal(18,5)
+                            , descuento  decimal(18,5)
+                            , impuestos  decimal(18,5)
+                            , creditos   decimal(18,5)
+                            , moneda     varchar(3)
+                            , pos        SMALLINT
+                            , is_walkout BOOLEAN
+                 );
