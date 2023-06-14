834f0d0201746a2a62e7c911dc2bfa292efa9548
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-14 20:09:58 -0300

    14/6: Se incluye ddl

--- parte-3/project/retail-data-warehouse/dim/dim_employee.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_employee.sql
@@ -1,1 +1,14 @@
 
+CREATE TABLE dim.employees (
+    id serial PRIMARY KEY,
+    nombre varchar(255),
+    apellido varchar(255),
+    fecha_entrada date,
+    fecha_salida date,
+    telefono varchar(255),
+    pais varchar(255),
+    provincia varchar(255),
+    codigo_tienda varchar(255),
+    posicion varchar(255)
+)
+;
