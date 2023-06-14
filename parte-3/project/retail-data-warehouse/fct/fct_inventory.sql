9c831c3a2557881e0de8474ff499ce41207a9f6a
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-14 20:19:51 -0300

    14/6: ddl

--- parte-3/project/retail-data-warehouse/fct/fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_inventory.sql
@@ -1,1 +1,9 @@
 
+CREATE TABLE fct.inventory
+                 (
+                              tienda  SMALLINT
+                            , sku     VARCHAR(10)
+                            , fecha   DATE
+                            , inicial SMALLINT
+                            , final   SMALLINT
+                 );
