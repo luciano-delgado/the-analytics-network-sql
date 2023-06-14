4228bbe9d8e834e8748a52e68d0611e107d93546
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-14 20:22:18 -0300

    14/6: ddl

--- parte-3/project/retail-data-warehouse/fct/fct_return_movements.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_return_movements.sql
@@ -1,1 +1,13 @@
 
+CREATE TABLE fct.return_movements
+                 (
+                              orden      	VARCHAR(10)
+                            , envio   		VARCHAR(10)
+			    , item 	        VARCHAR(10)
+                            , cantidad   	int
+			    , id_movimiento int  
+			    , desde 	        VARCHAR(50)
+			    , hasta 		VARCHAR(50)
+			    , recibido_por 	VARCHAR(10)
+                            , fecha      	date
+                 );
