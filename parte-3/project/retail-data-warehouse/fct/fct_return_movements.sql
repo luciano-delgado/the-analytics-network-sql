722dde50dca0104ed70b7022224f221ebd4423c8
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-14 21:27:06 -0300

    14/6: ddl + PK + FK

--- parte-3/project/retail-data-warehouse/fct/fct_return_movements.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_return_movements.sql
@@ -1,13 +1,17 @@
 
+
+DROP TABLE IF EXISTS fct.return_movements; 
 CREATE TABLE fct.return_movements
                  (
-                              orden      	VARCHAR(10)
-                            , envio   		VARCHAR(10)
-			    , item 	        VARCHAR(10)
-                            , cantidad   	int
-			    , id_movimiento int  
-			    , desde 	        VARCHAR(50)
-			    , hasta 		VARCHAR(50)
-			    , recibido_por 	VARCHAR(10)
-                            , fecha      	date
+				auto_id SERIAL PRIMARY KEY
+				, orden      	VARCHAR(10)
+				, envio   		VARCHAR(10)
+				, item 	        VARCHAR(10)
+				, cantidad   	int
+				, id_movimiento int  
+				, desde 	        VARCHAR(50)
+				, hasta 		VARCHAR(50)
+				, recibido_por 	VARCHAR(10)
+				, fecha      	date
+				, CONSTRAINT fk_item FOREIGN KEY (item) REFERENCES dim.product_master(codigo_producto)
                  );
