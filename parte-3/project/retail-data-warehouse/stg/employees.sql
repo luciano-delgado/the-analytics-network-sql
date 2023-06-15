c6199cbf0fe556481cbf0dc701866f08eacf6b12
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-14 21:46:05 -0300

    14/6: ddl + PK + Alter + SDC (WIP)

--- parte-3/project/retail-data-warehouse/stg/employees.sql
+++ parte-3/project/retail-data-warehouse/stg/employees.sql
@@ -1,5 +1,7 @@
-CREATE TABLE stg.employees (
-    -- id serial PRIMARY KEY, => En STG no van con CONSTRAINTS!
+--  Creo tabla empleados
+DROP TABLE IF EXISTS dim.employees;
+CREATE TABLE dim.employees (
+    auto_id SERIAL PRIMARY KEY
     nombre varchar(255),
     apellido varchar(255),
     fecha_entrada date,
@@ -11,3 +13,9 @@ CREATE TABLE stg.employees (
     posicion varchar(255)
 )
 ;
+-- Altero tabla para que soporte SDC
+ALTER TABLE dim.employees
+ADD COLUMN is_active bool
+-- QUEDA PENDIENTE SDC PARA UNIR CON FCT
+
+select * from dim.employees
