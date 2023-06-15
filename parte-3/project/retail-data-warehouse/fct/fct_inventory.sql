dfcc4f542317ca4aa2457627fe30c80d7e8fe4fe
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-14 21:14:53 -0300

    14/6: ddl + PK + FK

--- parte-3/project/retail-data-warehouse/fct/fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_inventory.sql
@@ -1,9 +1,19 @@
-
+DROP TABLE IF EXISTS fct.inventory;
 CREATE TABLE fct.inventory
                  (
-                              tienda  SMALLINT
-                            , sku     VARCHAR(10)
-                            , fecha   DATE
-                            , inicial SMALLINT
-                            , final   SMALLINT
-                 );
+					 auto_id SERIAL PRIMARY KEY,
+                     tienda  SMALLINT,
+					 sku     VARCHAR(10),
+					 fecha   DATE,
+					 inicial SMALLINT,
+					 final   SMALLINT,
+					 CONSTRAINT fk_inventory_sku
+					 FOREIGN KEY (sku)
+					 REFERENCES dim.product_master,
+					 CONSTRAINT fk_inventory_tienda
+					 FOREIGN KEY (tienda)
+					 REFERENCES dim.store_master
+                 )
+				 ;
+				 
+				
