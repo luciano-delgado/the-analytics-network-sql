d60042e58856952423d24799be38f7db3cfb3d1a
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-14 21:30:07 -0300

    14/6:ddl + PK + FK

--- parte-3/project/retail-data-warehouse/fct/fct_super_store_count.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_super_store_count.sql
@@ -1,7 +1,9 @@
-
+DROP TABLE IF EXISTS fct.super_store_count; 
 CREATE TABLE fct.super_store_count
-                 (
-                              tienda SMALLINT
-                            , fecha  VARCHAR(10)
-                            , conteo SMALLINT
-                 );
+(
+auto_id SERIAL PRIMARY KEY
+,tienda SMALLINT
+, fecha  VARCHAR(10)
+, conteo SMALLINT
+, CONSTRAINT fk_tienda FOREIGN KEY (tienda) REFERENCES dim.store_master(codigo_tienda)
+);
