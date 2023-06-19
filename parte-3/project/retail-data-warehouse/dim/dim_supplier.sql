c5be233450f77fb83de8d7b24831a4d97131f7e4
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-19 17:44:15 -0300

    19/6: ddl (sin PK)

--- parte-3/project/retail-data-warehouse/dim/dim_supplier.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_supplier.sql
@@ -1,8 +1,9 @@
 
 /*Crear tabla de suppliers*/
+/*No encontré una PK ya que los registros de codigo_producto y nombre del proveedor aparecen por duplicado.*/
 DROP TABLE IF EXISTS dim.suppliers; 
 CREATE TABLE dim.suppliers(
-	codigo_producto varchar(255) PRIMARY KEY,
+	codigo_producto varchar(255),
 	nombre varchar(255),
 	is_primary boolean
 );
