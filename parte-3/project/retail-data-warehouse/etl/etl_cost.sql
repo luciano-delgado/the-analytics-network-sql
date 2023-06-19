dd0aab41be1c4dc9edfd8d6076ac7073675932f7
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-19 20:47:06 -0300

    19/6: SP stg-> dim.cost

--- parte-3/project/retail-data-warehouse/etl/etl_cost.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_cost.sql
@@ -1,7 +1,7 @@
 /*CREACION DEL SP (EL CUAL SE ALMACENA EN ETL) QUE TOMARÁ LOS DATOS DEL STG Y LLENARÁ DIM 
 Agregado: Se agrega call.log() al final para que deje registro de fecha, tabla modificada y usuario
 */
-create or replace procedure etl.dim_cost()
+create or replace procedure etl.sp_dim_cost()
 language plpgsql as $$
 DECLARE
   usuario varchar(10) := current_user ;
