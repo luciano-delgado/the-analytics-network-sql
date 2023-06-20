9c59b93c2b04397ca14ef19ff84ed20246d1fb61
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-19 22:06:17 -0300

    19/6:SP stg->fct.order_line_sales

---
+++ parte-3/project/retail-data-warehouse/etl/sp_fct_order_line_sales.sql
@@ -0,0 +1,13 @@
+create or replace procedure etl.sp_order_line_sales()
+language sql 
+as $$ 
+truncate fct.order_line_sales;
+insert into fct.order_line_sales 
+select orden, producto, tienda, fecha, cantidad, venta, descuento, impuestos, creditos, moneda, pos, is_walkout
+from stg.order_line_sales ols 
+/*Hago inner join ya que OLS tiene productos que no estan en product_master*/
+inner join dim.product_master pm on pm.codigo_producto = ols.producto;
+call etl.sp_log(current_date,'fct.order_line_sales' ,'luciano');
+$$ 
+;
+call  etl.sp_order_line_sales()
