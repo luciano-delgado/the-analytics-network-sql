b081471f183d856ec54231e6048e0b2d91fa006c
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-20 11:54:24 -0300

    20/6: SP stg->fct.market_count (contemplando auto_id)

--- parte-3/project/retail-data-warehouse/etl/sp_market_count.sql
+++ parte-3/project/retail-data-warehouse/etl/sp_market_count.sql
@@ -3,7 +3,7 @@ language sql
 as $$ 
 truncate fct.market_count;
 insert into fct.market_count (tienda, fecha, conteo) --> Especifico columnas ya que fct.market_count tiene la 1er columna SERIAL ID
-select * from stg.market_count;
+select * from stg.market_count; 
 call etl.sp_log(current_date,'fct.market_count' ,'luciano');
 $$ 
 ;
