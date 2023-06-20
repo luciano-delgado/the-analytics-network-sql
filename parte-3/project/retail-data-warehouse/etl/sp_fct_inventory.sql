69cafad2bd4ae28ce7e9738763dc69dc4d921397
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-20 11:52:02 -0300

    20/6: SP stg->fct.inventory (contemplando auto_id)

--- parte-3/project/retail-data-warehouse/etl/sp_fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/etl/sp_fct_inventory.sql
@@ -2,10 +2,10 @@ create or replace procedure etl.sp_inventory()
 language sql 
 as $$ 
 truncate fct.inventory;
-insert into fct.inventory 
+insert into fct.inventory (tienda, sku, fecha, inicial, final) --> Especifico columnas ya que fct.market_count tiene la 1er columna SERIAL ID
 select * from stg.inventory;
 call etl.sp_log(current_date,'fct.inventory' ,'luciano');
 $$ 
 ;
--- call etl.inventory()
+call etl.inventory()
 
