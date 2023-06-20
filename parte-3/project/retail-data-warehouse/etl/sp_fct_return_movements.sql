e71ff9bf1428b5203486d11b30bfde6166d9e2d8
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-20 12:15:49 -0300

    20/6: stg->fct.return_movements (contemplando auto_id)

---
+++ parte-3/project/retail-data-warehouse/etl/sp_fct_return_movements.sql
@@ -0,0 +1,11 @@
+create or replace procedure etl.sp_return_movements()
+language sql 
+as $$ 
+truncate fct.return_movements;
+insert into fct.return_movements (orden, envio, item, cantidad, id_movimiento, desde, hasta, recibido_por, fecha) --> Especifico ya que la 1er col es el SERIAL ID
+select * from stg.return_movements;
+call etl.sp_log(current_date,'fct.return_movements' ,'luciano');
+$$ 
+;
+call etl.sp_return_movements()
+;
