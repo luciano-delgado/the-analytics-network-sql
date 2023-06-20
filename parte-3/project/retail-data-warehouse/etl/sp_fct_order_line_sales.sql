5f46113022a977c4b20025050e50eebf7b1f4f26
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-20 12:16:45 -0300

    20/6: SP stg->fct.order_line_sales (contemplando auto_id)

--- parte-3/project/retail-data-warehouse/etl/sp_fct_order_line_sales.sql
+++ parte-3/project/retail-data-warehouse/etl/sp_fct_order_line_sales.sql
@@ -5,7 +5,7 @@ truncate fct.order_line_sales;
 insert into fct.order_line_sales 
 select orden, producto, tienda, fecha, cantidad, venta, descuento, impuestos, creditos, moneda, pos, is_walkout --> Especifico columnas ya que la 1ra tiene SERIAL ID
 from stg.order_line_sales ols 
-/*Hago inner join ya que OLS tiene productos que no estan en product_master*/
+/*Hago inner join ya que OLS tiene productos que no estan en product_master*/ 
 inner join dim.product_master pm on pm.codigo_producto = ols.producto;
 call etl.sp_log(current_date,'fct.order_line_sales' ,'luciano');
 $$ 
