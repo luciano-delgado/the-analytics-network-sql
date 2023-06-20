9437a54fbe6307bb85e34b32630faf22f4a811ce
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-20 12:52:13 -0300

    20/6: SP stg->dim.employees (UPSERT + SUBROGATE KEY)

--- parte-3/project/retail-data-warehouse/etl/sp_dim_employees.sql
+++ parte-3/project/retail-data-warehouse/etl/sp_dim_employees.sql
@@ -1,7 +1,10 @@
+-- Hago nombre y apellido UNIQUE para concatenarlos + hashearlos + hacerlos PK (SUBROGATE KEY)
+ALTER TABLE dim.employees
+ADD CONSTRAINT nombre_apellido_unique UNIQUE (nombre, apellido);
+;
+
 create or replace procedure etl.sp_employees()
-language sql 
-as $$ 
-truncate dim.employees;
+language sql as $$ 
 insert into dim.employees 
 select 
   nombre, 
@@ -16,7 +19,13 @@ select
 case when fecha_salida is null then true else false end, -- is_active
 case when fecha_salida is null then null else cast(extract(month from fecha_salida) as int)-cast(extract(month from fecha_entrada) as int) end -- duration
 from stg.employees
+on conflict (nombre, apellido) do update 
+set 
+id = hashtext(concat(excluded.nombre,excluded.apellido)),
+duration = cast(extract(month from excluded.fecha_salida) as int)-cast(extract(month from excluded.fecha_entrada) as int)
+;
 call etl.sp_log(current_date,'dim.employees' ,'luciano');
 $$ 
 ;
 call etl.sp_employees()
+select * from dim.employees
