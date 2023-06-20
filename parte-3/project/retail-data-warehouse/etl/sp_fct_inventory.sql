9c95de4d499fa9024c550bb03e37404da69e8f4e
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-19 21:18:04 -0300

    19/6: SP stg->fct.inventory

--- parte-3/project/retail-data-warehouse/etl/sp_fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/etl/sp_fct_inventory.sql
@@ -3,7 +3,8 @@ language sql
 as $$ 
 truncate fct.inventory;
 insert into fct.inventory 
-select * from stg.inventory 
+select * from stg.inventory;
+call etl.sp_log(current_date,'fct.inventory' ,'luciano');
 $$ 
 ;
 -- call etl.inventory()
