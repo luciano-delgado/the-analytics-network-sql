9aad5db9a6e37c889b620051dc33abc20d6126cf
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-20 11:53:52 -0300

    20/6: SP stg->fct.super_store_count (contemplando auto_id)

--- parte-3/project/retail-data-warehouse/etl/sp_fct_super_store_count.sql
+++ parte-3/project/retail-data-warehouse/etl/sp_fct_super_store_count.sql
@@ -2,7 +2,7 @@ create or replace procedure etl.sp_super_store_count()
 language sql 
 as $$ 
 truncate fct.super_store_count;
-insert into fct.super_store_count
+insert into fct.super_store_count (tienda, fecha, conteo) --> Especifico ya que la 1er columna es SERIAL ID
 select * 
 from stg.super_store_count;
 call etl.sp_log(current_date,'fct.super_store_count' ,'luciano');
