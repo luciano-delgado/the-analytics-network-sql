2cb48b6b6f74f06f6438dc37738191218a1a82d9
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-19 21:44:30 -0300

    19/6: SP stg->dim.suppliers

---
+++ parte-3/project/retail-data-warehouse/etl/sp_suppliers.sql
@@ -0,0 +1,12 @@
+create or replace procedure etl.suppliers()
+language sql 
+as $$ 
+truncate dim.suppliers;
+insert into dim.suppliers 
+select * from stg.suppliers where is_primary = true ;
+call etl.sp_log(current_date,'dim.suppliers' ,'luciano');
+$$ 
+;
+call  etl.suppliers()
+;
+select * from dim.suppliers
