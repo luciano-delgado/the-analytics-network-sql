5184a263f9781c492c953afe1ce9951374c61d84
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-20 12:17:00 -0300

    (File) 20/6: SP stg->fct.return_movements (contemplando auto_id)

--- parte-3/project/retail-data-warehouse/etl/sp_fct_return_movements.sql
+++ parte-3/project/retail-data-warehouse/etl/sp_fct_return_movements.sql
@@ -1,5 +1,5 @@
 create or replace procedure etl.sp_return_movements()
-language sql 
+language sql  
 as $$ 
 truncate fct.return_movements;
 insert into fct.return_movements (orden, envio, item, cantidad, id_movimiento, desde, hasta, recibido_por, fecha) --> Especifico ya que la 1er col es el SERIAL ID
