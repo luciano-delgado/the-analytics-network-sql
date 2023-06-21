b568984b4aeebc388e7620584c22987325cc5cff
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-21 20:54:57 -0300

    21/6: ddl

--- parte-3/project/retail-data-warehouse/stg/return_movements.sql
+++ parte-3/project/retail-data-warehouse/stg/return_movements.sql
@@ -1,5 +1,5 @@
 CREATE TABLE stg.return_movements
-                 (
+                 ( 
                               orden      	VARCHAR(10)
                             , envio   		VARCHAR(10)
 			    , item 	        VARCHAR(10)
