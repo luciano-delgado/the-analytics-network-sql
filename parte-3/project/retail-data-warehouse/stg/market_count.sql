672a4a6d891236874ed2afc8cb8565bee1422b12
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-21 20:54:01 -0300

    21/6: ddl

--- parte-3/project/retail-data-warehouse/stg/market_count.sql
+++ parte-3/project/retail-data-warehouse/stg/market_count.sql
@@ -1,5 +1,5 @@
 CREATE TABLE stg.market_count
-                 (
+                 ( 
                               tienda SMALLINT
                             , fecha  INTEGER
                             , conteo SMALLINT
