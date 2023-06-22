686c501a4e1fb882d2047d0e9e640e6e4504e8a2
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-06-21 21:24:10 -0300

    21/6: SP Logging

--- parte-3/project/retail-data-warehouse/etl/sp_log.sql
+++ parte-3/project/retail-data-warehouse/etl/sp_log.sql
@@ -1,17 +1,26 @@
+
 /*CREO TABLA DE LOGS*/
-CREATE OR REPLACE TABLE etl.sp_log
+CREATE table etl.sp_log
                  (
-                              fecha date
-                            , tabla          VARCHAR(255)
-                            , usuario       VARCHAR(255)
-					 )
+fecha date
+, tabla          VARCHAR(255)
+, usuario       VARCHAR(255)
+, stored_procedure       VARCHAR(255)
+, lineas_insertadas       int
+					 
+			 );
 /*CREO PROCEDURE QUE LLENA LA TABLA EN SCHEMA ETL*/
-create or replace procedure etl.log(parametro_fecha date, parametro_tabla varchar(10), parametro_usuario varchar(10))
+create or replace procedure etl.log(parametro_fecha date, 
+				parametro_tabla varchar(10), 
+				parametro_usuario varchar(10),
+				parametro_sp varchar(10),
+				parametro_lineas int
+				   )
 language sql as $$
-insert into etl.log (fecha,tabla,usuario) 
-select parametro_fecha, parametro_tabla, parametro_usuario
+insert into etl.sp_log 
+select parametro_fecha, parametro_tabla, parametro_usuario, parametro_sp, parametro_lineas
 ; 
 $$;
+-- select * from etl.sp_log
+-- call etl.log('2023-05-26','dim.cost','lucho', 'prueba',1)
 
---call etl.log('2023-05-26','dim.cost','lucho')
---select * from etl.log
