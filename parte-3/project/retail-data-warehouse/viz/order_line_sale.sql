8e1249ff3c22fcaba7a8f456c95728e0e1ac29e7
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-07-02 19:06:09 -0300

    20/6: Creo tabla e inserto datos

--- parte-3/project/retail-data-warehouse/viz/order_line_sale.sql
+++ parte-3/project/retail-data-warehouse/viz/order_line_sale.sql
@@ -1,3 +1,3 @@
-# Creo la tabla tomando el integrador final del TP2
+# --TP3-P5.1-order_line_sale: Creo la tabla tomando el integrador final del TP2
 create table viz.order_line_sale as 
 select * from stg.integrador_2_final
