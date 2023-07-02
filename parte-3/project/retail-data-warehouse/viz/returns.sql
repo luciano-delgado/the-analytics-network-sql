08bb1399c26e15ac9ffbfde5d0fb9084f80bda19
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-07-02 15:41:40 -0300

    2/7: Se crea tabla con inserción de datos

---
+++ parte-3/project/retail-data-warehouse/viz/returns.sql
@@ -0,0 +1,51 @@
+-- ## Funcion MFX rate -- ## 
+create function stg.convert_usd(moneda varchar(3),valor decimal(18,5), fecha date) returns decimal(18,5) 
+as $$
+select 
+coalesce(round(valor/(case 
+	when moneda = 'EUR' then mfx.cotizacion_usd_eur
+	when moneda = 'ARS' then mfx.cotizacion_usd_peso
+	when moneda = 'URU' then mfx.cotizacion_usd_uru
+	else 0 end),2),0) as valor_usd
+from stg.monthly_average_fx_rate mfx 
+where extract(month from mfx.mes) = extract(month from fecha)
+;
+
+-- ## Creo tabla Returns en VIZ e inserto datos -- ##
+create table viz.returns(
+	orden varchar(10),
+	venta numeric(18,5),
+	venta_usd numeric,
+	producto varchar(10),
+	nombre varchar(255),
+	categoria varchar(255),
+	subcategoria varchar(255),
+	origen varchar(255),
+	material varchar(255),
+	tienda smallint, 
+	ciudad varchar(255),
+	nombre_tienda varchar(255),
+	fecha_apertura date
+)
+;
+select * from viz.returns
+;
+insert into viz.returns
+select 
+  ols.orden, 
+  venta,
+  stg.convert_usd('ARS',venta, ols.fecha) as venta_usd,
+  producto, 
+  pm.nombre, 
+  pm.categoria,
+  pm.subcategoria,
+  pm.origen,
+  pm.material,
+  tienda,
+  sm.ciudad,
+  sm.nombre as nombre_tienda,
+  sm.fecha_apertura
+from bkp.order_line_sale_20230526 ols
+  inner join (select * from bkp.return_movements_20230526 rm1 where rm1.desde = 'Cliente' ) rm on rm.orden = ols.orden  
+  left  join bkp.store_master_20230526 sm on sm.codigo_tienda = ols.tienda
+  left join bkp.product_master_20230526 pm on pm.codigo_producto = ols.producto
