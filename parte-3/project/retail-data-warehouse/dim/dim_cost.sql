6adc18d1777cdc5ed6c18ec5453627b152c5252e
Lucho <lucianodelgado92@gmail.com>
2023-07-08 18:34:38 -0300

    8/7: FK optional
    Se agrega respuesta a si es necesario usar FK en cost y se prueba Git desktop

--- parte-3/project/retail-data-warehouse/dim/dim_cost.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_cost.sql
@@ -7,3 +7,10 @@ cost_usd numeric
 
 ### ver si debe llevar FK con respecto a la product_master. ¿Puedo tener costo de un producto que no esté en maestro?
 
+"No tiene mucho sentido PK a tablas fact, se suele agregar una columna identidad autogenerada pero no es obligatorio
+Tema dim_cost si efectivamente puede que tenga PK y FK. 
+
+Todo depende de cuanto querramos restringir la presencia de datos no correctos. Lo mas probable es que tu fuente sea la dim_product y la dim_cost sea un left join en todo momento. Por lo tanto no tendrias problemas. 
+
+Pero se puede agregar la constraint de FK."
+
