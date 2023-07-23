21885d8857338681a275898621e78304c2e152b4
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-07-23 18:24:13 -0300

    23/7: Corrección según devolución

--- parte-3/project/retail-data-warehouse/dim/dim_mfx_rate.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_mfx_rate.sql
@@ -2,13 +2,23 @@
 /* Crea tabla monthly_average_fx_rate
 Promedio de cotizacion mensual de USD a ARS, EUR a ARS y USD a URU
 */
-DROP TABLE IF EXISTS dim.monthly_average_fx_rate;
+
+-- DROP TABLE IF EXISTS dim.monthly_average_fx_rate;
     
-CREATE TABLE dim.monthly_average_fx_rate
-                 (
-                              mes                 DATE PRIMARY KEY
-                            , cotizacion_usd_peso DECIMAL
-                            , cotizacion_usd_eur DECIMAL
-                            , cotizacion_usd_uru  DECIMAL
-                 );
+-- CREATE TABLE dim.monthly_average_fx_rate
+--                  (
+--                               mes                 DATE PRIMARY KEY
+--                             , cotizacion_usd_peso DECIMAL
+--                             , cotizacion_usd_eur DECIMAL
+--                             , cotizacion_usd_uru  DECIMAL
+--                  );
  
+-- 23/7: Corrección indicada por Agus el 16/7, 
+CREATE TABLE dim.monthly_average_fx_rate
+(
+    mes DATE,
+    cotizacion_usd_peso DECIMAL,
+    cotizacion_usd_eur DECIMAL,
+    cotizacion_usd_uru DECIMAL,
+    PRIMARY KEY (extract(month from mes)) -- Nuevo!
+);
