4dea0dfc6ad18b3e5dd5b81ce0a5e4c8e497709a
Lucho <62969433+luciano-delgado@users.noreply.github.com>
2023-07-23 18:50:03 -0300

    23/7: Corrección según devolución
    En Git guardar el create del SP y eliminar el resto para no agregar confusion. Ya que luego el SP se ejecuta desde un scheduler.
    
    Inventory: En aquellos modelos donde insertas por fecha a veces se suele eliminar primero los registros de esa fecha en adelante. La teoría dice que no deberías eliminar nada pero es como método de asegurarse que no haya duplicados.

--- parte-3/project/retail-data-warehouse/etl/sp_fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/etl/sp_fct_inventory.sql
@@ -1,6 +1,10 @@
 create
 or replace procedure etl.sp_inventory () language sql as $ $ truncate fct.inventory;
 
+DELETE FROM fct.inventory
+WHERE fecha >= current_date -- 23/7: Elimino registros de la fecha de inserción en adelante según corrección de Agus 16/7
+    ;
+
 insert into
     fct.inventory (tienda, sku, fecha, inicial, final) --> Especifico columnas ya que fct.market_count tiene la 1er columna SERIAL ID
 select
